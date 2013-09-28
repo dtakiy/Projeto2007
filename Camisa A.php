d0e9a4cf3d7d35e63055407f03b3cab0e3aedee3
Daniel Franco <tsuba12345@yahoo.com.br>
2013-09-27 22:08:09 -0300

    adicionando algumas figuras e paginas

---
+++ Camisa A.php
@@ -0,0 +1,158 @@
+  
+<html xmlns='http://www.w3.org/1999/xhtml'>
+<head>
+<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+<link rel='icon' rel='icon' href='favicon.ico' type='image/ico' />
+<link rel='stylesheet' type='text/css' href='estilo.css' />
+<title>Cusko</title>
+</head>
+<body>
+    <div class='global-div'>
+    	<div class='topo-div'></div>
+        <div class='menu-div'>
+        	<ul>
+        	<ul>
+			  	<table>
+			  	<td>&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+			  	&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;
+			  	&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;
+			  	&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;
+			  	&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;
+			    <img src='cusko_branco.jpg' 'height='100' width='200'> </td>
+			  	</table>
+			  	<br>
+			  	<br>
+<div id='cssmenu'>
+<ul>
+  <li><a href='index.php'><span>Home</span></a></li>
+  <li><a href='produtos.php'><span>Produtos</span></a></li>
+  <li><a href='quemsomos.php'><span>Quem Somos</span></a></li>
+  <li><a href='contato.php'><span>Contato</span></a></li>
+  <li><a href='carr.php'><span>Carrinho</span></a></li>
+</ul>
+</div>				                          
+            </ul>
+        </div>
+        <div class='esq-div'>
+        	<div class='destaques2-div'>
+           <h5>Camisa A Tamanho M </h5>
+			
+			<table border=1 style='text-align:right'>
+<td>
+<table border=0>
+<BR>
+<BR>
+<td><img src='camisa2.jpg'height='150' width='150' name='prod'>&nbsp &nbsp</td>
+<td><table border=0>
+<td>Dimensoes:(cm)</td><td>&nbsp;&nbsp;P&nbsp;&nbsp;</td><td>&nbsp;&nbsp;M&nbsp;&nbsp;</td><td>&nbsp;&nbsp;G&nbsp;&nbsp;</td>
+<tr><td>Manga</td><td>&nbsp;17,5&nbsp;</td><td>&nbsp;17,5&nbsp;</td><td>&nbsp;18,5&nbsp;</td></tr>
+<tr><td>Largura</td><td>46,5</td><td>49,5</td><td>50</td></tr>
+<tr><td>Comprimento</td><td>66,5</td><td>69,5</td><td>71</td></tr>
+</table></td>
+
+</table>
+</td>
+
+
+<tr>
+<tr><td>Descrição</td></tr>
+<tr><td><textarea rows='10' cols='40' disabled>
+teste
+</textarea></tr></td>
+<tr>  <td> <!--Tamanho
+&nbsp&nbsp&nbsp<select name='tamprod2'>
+<option value='1' selected>Selecione</option>
+<option value='2' >P</option>
+<option value='3' >M</option>
+<option value='4' >G</option>
+</select> -->
+&nbsp&nbsp Preço &nbsp; &nbsp; R$ &nbsp;1,00
+</td> 
+
+</tr><!--
+<td>Quantidade
+<select name='tamprod2'>
+<option value='0' selected>Selecione</option>
+<option value='1' >1</option>
+<option value='2' >2</option>
+<option value='3' >3</option>
+<option value='4' >4</option>
+<option value='4' >5</option>
+</select>-->
+<td><div align='center' style='font-size:10px;font-family:Verdana'><a href='carr.php?cod=13&acao=incluir' class='button'>Comprar</a></div><br></td>
+</td>
+</tr>
+</table>
+
+<div class='fb-comments' data-href='127.0.0.1/cusko/Camisa A.php' data-width='470' data-num-posts='10'></div>
+     	
+<BR>
+<BR>
+		
+<div class='fb-like' data-href='https://pt-br.facebook.com/usecusko' data-send='true' data-layout='button_count' 
+data-width='450' data-show-faces='true' data-font='arial'></div>
+ </div>
+        
+        
+			
+
+<div class='rodape-div'><p>Loja Cusko</p></div>		
+</div>
+         <div class='dir-div'>								
+            <h4>Menu</h4>
+            <br>
+            <div id='menuvert'>
+			<ul>
+
+            <ul class='maisartigos escuro top8'>
+            
+            <li><a href='#' title='Camisas Masculinas'>Camisas Masculinas</a></li>
+            <li><a href='#' title='Camisas Femininas'>Camisas Femininas</a></li>
+            <li><a href='#' title='Calcas Masculinas'>Calças Masculinas</a></li>
+            <li><a href='#' title='Calcas Femininas'>Calças Femininas</a></li>
+            <li><a href='#' title='Bermudas Masculinas'>Bermudas Masculinas</a></li>
+            <li><a href='#' title='Shorts Femininos'>Shorts Femininos</a></li>
+			<li><a href='#' title='Acessorios'>Acessórios</a></li>
+			<br>   
+			     
+            </ul>
+            </div>
+            <br>
+            <h4>Busca De Produtos</h4>
+            <br>      
+        	<input name='login' type='text' id='login' placeholder='Nome ou Descrição' size='20' maxlength='60'/>
+        	<input name='btnsearchprod' class='button' type='submit' size='2' id='btnsearchprod' value='Buscar' />
+        	<br>
+        	<br>
+        	<h4>Consultar Pedido</h4>
+            <BR>
+			<form id='formconsult' name='formconsult' method='post' action='consulta.php'>
+      		<table border=0  width=auto height=auto> 
+      		<input name='login' type='text' id='login' placeholder='Código da Compra' size='20' maxlength='60'/>
+      		<input name='btn_codcompra' class='button' type='submit' size='2' id='btn_codcompra' value='Pesquisar' />
+
+      		</table>
+      		</form>		  
+        	
+        	<!-- Like Button Facebook -->
+        	<div id='fb-root'></div>        	
+			<div class='fb-like' data-href='https://pt-br.facebook.com/usecusko' data-send='true' data-layout='button_count' 
+			data-width='450' data-show-faces='true' data-font='arial'></div>
+			<!-- Fallow Button Facebook -->
+			<div class='fb-follow' data-href='https://www.facebook.com/usecusko' data-width='450' data-layout='button_count' data-show-faces='true'></div>
+		    </div>
+		
+</div>
+				<!-- Script para se conectar ao Facebook -->
+				<script>(function(d, s, id) {
+  				var js, fjs = d.getElementsByTagName(s)[0];
+  				if (d.getElementById(id)) return;
+ 					js = d.createElement(s); js.id = id;
+  				js.src = '//connect.facebook.net/en_US/all.js#xfbml=1';
+  				fjs.parentNode.insertBefore(js, fjs);
+				}(document, 'script', 'facebook-jssdk'));</script>
+</body>
+</html>
+
+
+
---
+++ calcafem.php
@@ -0,0 +1,247 @@
+<?php
+session_start();
+echo "";
+//echo session_id();
+$usuario = $_SESSION['usrlogin']; 
+$email = $_SESSION['email'];
+$emailcripto = base64_decode($email);
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="estilo.css" />
+<link rel='icon' rel='icon' href='favicon.ico' type='image/ico' />
+<title>Cusko</title>
+</head>
+
+<body>
+    <div class="global-div">
+    	<div class="topo-div"></div>
+        <div class="menu-div">
+        	<ul>
+        	<ul>
+<?php
+if ($usuario !='')
+{
+echo "";
+}
+else{
+echo "
+        	    <table border=0>
+        		<form name='input' action='auth.php'method='post'>  
+        		<td><a  title='login'><h8>Login</h8> <input name='login' type='text' id='login' size='20' maxlength='40' /></td>
+				<td><a  title='senha'><h8>Senha</h8><input name='senha' type='password' id='senha' size='20' maxlength='40' /></td>
+				</a>
+			    <td><input name='btn_logar' class='button entrar' type='submit' id='btn_logar' value='Logar' /> </td>
+			    <td><a href='cadusradm.php' class='button'>Cadastrar</a></td>
+			    <td><a href='recsenha.php' class='button'>Esqueceu Senha?</a></td>
+			    </td>
+			    </table>	
+			  	</form>        	
+			  	<table>";
+}
+?>
+			  	<td>&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+			  	&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;
+			  	&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;
+			  	&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;
+			  	&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;
+			  	&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;
+			  	&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;
+			    <img src='cusko_branco.jpg' 'height='100' width='200'> </td>
+			  	
+			  	</table>
+			  	<br>
+			  	<br>
+		  				  	
+<div id='cssmenu'>
+<ul>
+  <li><a href='index.php'><span>Home</span></a></li>
+  <li><a href='produtos.php'><span>Produtos</span></a></li>
+  <li><a href='quemsomos.php'><span>Quem Somos</span></a></li>
+  <li><a href='contato.php'><span>Contato</span></a></li>
+  <li><a href='carr.php'><span>Carrinho</span></a></li>
+  
+  <?php
+if ($usuario !='')
+{
+echo "<form id=formlogout' name='formlogout' method='post'  action='logout.php'>";
+echo "<font size='2.5' color='white'>" .$emailcripto;
+echo "</font>";
+echo "<input name='btn_logout' class='button' type='submit' id='btn_logout' value='Logout' size='40'  />";
+echo "</form>";
+}
+else{
+echo "";
+}
+?>
+  
+</ul>
+</div>				                          
+            </ul>
+        </div>
+        
+        <div class="esq-div">
+		      
+        	<div class="destaques-div">
+            <h5>Calças Femininas</h5>
+			
+		<?php
+require_once("conf.php");
+
+$max=5; // numero maximo de produtos por pagina
+
+$pagina=$_GET["pagina"]; 
+if ($pagina == "")
+$pagina=1;
+ 
+$inicio = $pagina - 1;
+$inicio = $max * $inicio;
+ 
+  
+$sql="SELECT * FROM produtos where cat_produto=5 and status_prod=1 ";
+$res=mysql_query($sql);
+$total=mysql_num_rows($res);
+ 
+if ($total == 0)
+echo "Nenhum registro encontrado!";
+else
+{
+echo "<BR>";
+
+$sql="SELECT * FROM produtos where cat_produto=3 and status_prod=1 LIMIT $inicio,$max";
+$res=mysql_query($sql);
+while ($row = mysql_fetch_assoc($res)) {
+echo "<table border=0>";
+echo "<tr>";
+echo "<td>";
+
+			echo "
+			<br/><br/>
+			<table border=0>
+			<tr>
+			<form action='buscar.php' method='get'> <div align='center'><br>";
+	$preco=$row['preco_produto'];
+	// trocando . por ,
+	$precoprod = str_replace(".",",",$preco);
+
+	echo "<center>";
+	echo "<tr>";
+	echo "<td> <font size='2.5' color='black'>".$row['nome_produto'];
+	echo "</td>";
+	echo "</tr>";
+	echo "<br>";
+	echo "<tr><td><a href='".$row['nome_produto'].".php'><img src='".$row['imagem']."'height='130' width='130' name='eimg'></tr></td>";
+	echo "<td> <font size='2.5' color='black'>Preço R$ ".$precoprod;
+	echo "</tr>";
+
+	echo "</td>";
+	echo "</tr></td>";
+	echo "<tr>";
+	echo "<td><div align='center' style='font-size:10px;font-family:Verdana'><a href='carr.php?cod=".$row['idprodutos']."&acao=incluir' class='button'>Comprar</a></div><br></td>";
+	echo "</tr>";
+	echo "</center>";
+	echo "</table>";
+    echo "</form>"; 
+    echo "<td>";
+}
+	
+	for($i =1; $i <= $max; $i++){
+	echo "</table>";
+	}
+ 
+
+ 
+}
+// Calculando pagina anterior
+$menos = $pagina - 1;
+// Calculando pagina posterior
+$mais = $pagina + 1;
+$pgs = ceil($total / $max);
+if($pgs > 1 ) 
+{
+if($menos>0) 
+echo "<a href=\"?pagina=$menos\" class='texto_paginacao'>Anterior</a> "; 
+ 
+if (($pagina-4) < 1 )
+$anterior = 1;
+else
+$anterior = $pagina-4;
+ 
+if (($pagina+4) > $pgs )
+$posterior = $pgs;
+else
+$posterior = $pagina + 4;
+ 
+for($i=$anterior;$i <= $posterior;$i++) 
+if($i != $pagina) 
+echo " <a href=\"?pagina=".($i)."\" class='texto_paginacao'>$i</a>";
+else 
+echo " <strong class='texto_paginacao_pgatual'>".$i."</strong>";
+ 
+if($mais <= $pgs) 
+echo " <a href=\"?pagina=$mais\" class='texto_paginacao'>Proxima</a>";
+}
+	?>	
+        </div>
+		<div class="rodape-div"></div>		<!-- <p>Loja Cusko</p> caso queira colocar frase dentro do rodape -->
+		</div>
+         <div class="dir-div">								
+            <h4>Menu</h4>
+            <br>
+            <div id='menuvert'>
+			<ul>
+            <ul class="maisartigos escuro top8">
+            <li><a href="cammasc.php" title="Camisas Masculinas">Camisas Masculinas</a></li>
+            <li><a href="camfem.php" title="Camisas Femininas">Camisas Femininas</a></li>
+            <li><a href="calcmas.php" title="Calcas Masculinas">Calças Masculinas</a></li>
+            <li><a href="calcafem.php" title="Calcas Femininas">Calças Femininas</a></li>
+            <li><a href="bermasc.php" title="Bermudas Masculinas">Bermudas Masculinas</a></li>
+            <li><a href="shortfem.php" title="Shorts Femininos">Shorts Femininos</a></li>
+			<li><a href="acessorios.php" title="Acessorios">Acessórios</a></li>
+			<br>     
+            </ul>
+            </div>
+            <br>
+            <h4>Busca De Produtos</h4>
+            <BR>
+         	<form id="buscaprod" name="buscaprod" method="post" action="reqbusprod.php"> <!-- productlistclient.php web -->
+      	 	<table border=0  width=auto height=auto>   
+      		<td width=auto><input type="text" name="buscarprod" placeholder="Nome do Produto" size=35 maxlength=auto />
+      		<td> <input name="btn_cadusr" class="button" type="submit" id="btn_pesq_prod" value="Buscar" size=35  /> </td>
+      		</table>
+      		</form>
+        	<br>
+        	<br>
+        	<h4>Consultar Pedido</h4>
+            <BR>
+			<form id="formconsult" name="formconsult" method="post" action="consulta.php">
+      		<table border=0  width=auto height=auto> 
+      		<td width="auto"><input type="text" name="codcompra" id="codcompra" size="35" placeholder="Código da Compra" /> </td>
+      		<td><input name="btn_codcompra" class="button" type="submit" id="btn_codcompra" value="Pesquisar" size="35"  /></td>
+      		</table>
+      		</form>		  
+		      
+        	<!-- Like Button Facebook -->
+        	<div id="fb-root"></div>        	
+			<div class="fb-like" data-href="https://pt-br.facebook.com/usecusko" data-send="true" data-layout="button_count" 
+			data-width="450" data-show-faces="true" data-font="arial"></div>
+			<!-- Fallow Button Facebook -->
+			<div class="fb-follow" data-href="https://www.facebook.com/usecusko" data-width="450" data-layout="button_count" data-show-faces="true"></div>
+
+		
+		</div>
+
+</div>
+				<!-- Script para se conectar ao Facebook -->
+				<script>(function(d, s, id) {
+  				var js, fjs = d.getElementsByTagName(s)[0];
+  				if (d.getElementById(id)) return;
+ 					js = d.createElement(s); js.id = id;
+  				js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  				fjs.parentNode.insertBefore(js, fjs);
+				}(document, 'script', 'facebook-jssdk'));</script>
+</body>
+</html>
