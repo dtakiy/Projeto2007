5a413d472e783353ada0e281a4e0a308b1800906
Daniel Franco <tsuba12345@yahoo.com.br>
2013-11-02 01:36:25 -0200

    arrumando produtos por lista

--- produtos.php
+++ produtos.php
@@ -7,11 +7,12 @@ $email = $_SESSION['email'];
 $emailcripto = base64_decode($email);
 ?>
          <?php
+         require_once("conf.php");
          require_once('reader.php');	
-         ?>
+         ?> 
 </ul>
 </div>				                          
-            </ul>
+            </ul>            
         </div>
         <div class="esq-div">
         	<div class="destaques-div">
@@ -21,41 +22,34 @@ $emailcripto = base64_decode($email);
 			<BR>
 			<div align="center">
 			<table border=0>
-			<tr>
-			<td>Camisas Masculinas</td>
-			<td>Bermudas Masculinas</td>
-			<td>Calças Masculinas</td>
-			</tr>
-			<tr>
-			<td><a href='cammasc.php'><img src='camisa1.jpg'height='130' width='130' name='cammasc'></td>	
-			<td><a href='bermasc.php'><img src='bermasc1.png'height='130' width='130' name='bermasc'></td>
-			<td><a href='calcmas.php'><img src='calcmas1.png'height='130' width='130' name='calcmas'></td>
-			</tr>
-			</table>
-			<BR>
-			<BR>
+			<td><h6>Camisas Masculinas</h6></td>
+			<?php
+			echo "<td>";
+			$sql = mysql_query("SELECT * FROM produtos where cat_produto=2 and status_prod=1 and qtd_produto >0 ORDER BY nome_produto LIMIT 0, 3",$con); 
+			while ($row = mysql_fetch_assoc($sql)) {
+			$nomefig = $row['imagem'];
+			$nomelink = $row['nome_produto'];
+			echo "<td><a href='$nomelink.php'><img src='$nomefig'height='130' width='130' name='cammasc'></td>	";
+			echo "</td>";
+			}
+			echo "<td><a href='cammasc.php'><img src='plus.jpg'height='30' width='30' name='cammasc'></td>	";
+			echo "</table>";
+			?>
 			<table border=0>
-			<tr>
-			<td>Camisas Femininos</td>
-			<td>Shorts Femininos</td>
-			<td>Calças Femininos</td>
-			</tr>
-			<tr>
-			<td><a href='camfem.php'><img src='fem2.jpg'height='130' width='130' name='camfem'></td>
-			<td><a href='shortfem.php'><img src='shortsfem1.png'height='130' width='130' name='shortfem'></td>
-			<td><a href='calcafem.php'><img src='calcafem1.png'height='130' width='130' name='calcafem1'></td>
-			</tr>
-			</table>
-			<BR>
-			<BR>
-			<table border=0>
-			<tr>
-			<td>Acessórios</td>
-			</tr>
-			<tr>
-			<td><a href='acessorios.php'><img src='acce1.png'height='130' width='130' name='acessorios'></td>
-			</tr>
-			</table>
+			<td><h6>Camisas Femininas</h6></td>
+			<?php
+			echo "<td>";
+			$sql = mysql_query("SELECT * FROM produtos where cat_produto=3 and status_prod=1 and qtd_produto >0 ORDER BY nome_produto LIMIT 0, 3",$con); 
+			while ($row = mysql_fetch_assoc($sql)) {
+			$nomefig = $row['imagem'];
+			$nomelink = $row['nome_produto'];
+			echo "<td><a href='$nomelink.php'><img src='$nomefig'height='130' width='130' name='cammasc'></td>	";
+			echo "</td>";
+			}
+			echo "<td><a href='camfem.php'><img src='plus.jpg'height='30' width='30' name='cammasc'></td>	";
+			echo "</table>";
+			?>
+
 			</div>
         </div>
 <div class="rodape-div"><p>Loja Cusko</p></div>		
